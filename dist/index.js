faaed50200624c18f51549a79e9c9a7be65de2d2
BrucePaterson64 <bruce_paterson@sky.com>
2023-02-15 16:39:14 +0000

    update

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 // DOM elements
 const L1List = document.querySelector('.level1');
 const L2List = document.querySelector('.level2');
+const Lt1List = document.querySelector('.lights1');
+const Lt2List = document.querySelector('.lights2');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
 
@@ -49,6 +51,41 @@ const setupL1 = (data) => {
       document.getElementById("mainContainer").style.display='block';
       document.getElementById("loggedout").style.display='none';
       L2List.innerHTML = html;
+    } 
+};
+const setupLt1 = (data) => {
+  if (data.length) {
+    let html = '';
+    data.forEach(doc => {
+      const l1 = doc.data();
+      const li = `
+        <tr><td>${l1.check}</td><td>OK</td><td>Rfd</td><td>Sn</td><td>NA</td></tr>
+      `;
+    html += li;
+    });
+    document.getElementById("mainContainer").style.display='block';
+    document.getElementById("loggedout").style.display='none';
+    Lt1List.innerHTML = html
+  } else {
+    console.log("Logged Out");
+    document.getElementById("loggedout").style.display='block';
+    document.getElementById("mainContainer").style.display='none';
+    loggedout.innerHTML = '<h5 class="center-align">Login to view inspection report</h5>';
+  }
+};
+  const setupLt2 = (data) => {
+    if (data.length) {
+      let html = '';
+      data.forEach(doc => {
+        const l2 = doc.data();
+        const li = `
+          <tr><td>${l2.check}</td><td>OK</td><td>Rfd</td><td>Sn</td><td>NA</td></tr>
+        `;
+      html += li;
+      });
+      document.getElementById("mainContainer").style.display='block';
+      document.getElementById("loggedout").style.display='none';
+      Lt2List.innerHTML = html;
     } 
 };
 
